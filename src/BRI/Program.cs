9ecc52923b2e0f73a91457f0e5142ad8e6f4597b
Mattias Karlsson <matkar@wcom.se>
2025-01-20 16:40:17 +0100

    Migrate to use Devlead.Console

--- src/BRI/Program.cs
+++ src/BRI/Program.cs
@@ -1,61 +1,39 @@
 ﻿using BRI.Commands;
-using Microsoft.Extensions.Configuration.Memory;
-using Microsoft.Extensions.Configuration;
-using Spectre.Console.Cli.Extensions.DependencyInjection;
 using Azure.Core;
 using Azure.Identity;
 
-var serviceCollection = new ServiceCollection()
-    .AddCakeCore()
-    .AddLogging(configure =>
-            configure
-                .AddSimpleConsole(opts =>
-                {
-                    opts.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
-                })
-                .AddConfiguration(
-                new ConfigurationBuilder()
-                    .Add(new MemoryConfigurationSource
-                    {
-                        InitialData = new Dictionary<string, string?>
-                        {
-                            { "LogLevel:System.Net.Http.HttpClient", "Warning" }
-                        }
-                    })
-                    .Build()
-            ))
-    .AddSingleton<AzureTokenService>(
-        async () =>
-        {
-            var tokenCredential = new DefaultAzureCredential();
-            var accessToken = await tokenCredential.GetTokenAsync(
-                new TokenRequestContext(scopes: new string[] { "https://management.azure.com/.default" })
-            );
-            return accessToken;
-        }
-    )
-    .AddSingleton<BicepModuleMarkdownService>()
-    .AddSingleton<RepositoryService>()
-    .AddSingleton<CatalogService>()
-    .AddSingleton<TagService>()
-    .AddSingleton<ManifestService>()
-    .AddSingleton<BlobService>()
-    .AddSingleton<InventoryCommand>()
-    .AddSingleton<TokenService>();
-
-serviceCollection.AddHttpClient();
-
-using var registrar = new DependencyInjectionRegistrar(serviceCollection);
-var app = new CommandApp(registrar);
-
-app.Configure(
-    config =>
+public partial class Program
+{
+    static partial void AddServices(IServiceCollection services)
     {
-        config.ValidateExamples();
+        services
+            .AddCakeCore()
+            .AddSingleton<AzureTokenService>(
+                async () =>
+                {
+                    var tokenCredential = new DefaultAzureCredential();
+                    var accessToken = await tokenCredential.GetTokenAsync(
+                        new TokenRequestContext(scopes: ["https://management.azure.com/.default"])
+                    );
+                    return accessToken;
+                }
+            )
+            .AddSingleton<BicepModuleMarkdownService>()
+            .AddSingleton<RepositoryService>()
+            .AddSingleton<CatalogService>()
+            .AddSingleton<TagService>()
+            .AddSingleton<ManifestService>()
+            .AddSingleton<BlobService>()
+            .AddSingleton<InventoryCommand>()
+            .AddSingleton<TokenService>();
 
-        config.AddCommand<InventoryCommand>("inventory")
+        services.AddHttpClient();
+    }
+
+    static partial void ConfigureApp(AppServiceConfig appServiceConfig)
+    {
+        appServiceConfig.AddCommand<InventoryCommand>("inventory")
                 .WithDescription("Example inventory command.")
-                .WithExample(new[] { "inventory", "test.azurecr.io", "outputpath" });
-    });
-
-return await app.RunAsync(args);
+                .WithExample(["inventory", "test.azurecr.io", "outputpath"]);
+    }
+}
